0c18169c42aee1f6c24b1c5b78802e86d485eed3
StphanMueller <stephanm1@yahoo.de>
2018-03-15 11:37:02 +0100

    Update BankImporter.java
    Bundesbank-URL auf https umgestellt

--- blz-data/src/main/java/de/cpg/oss/blz/BankImporter.java
+++ blz-data/src/main/java/de/cpg/oss/blz/BankImporter.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Component
 public class BankImporter implements ResourceLoaderAware {
 
-    private final static String URL = "http://www.bundesbank.de/Redaktion/DE/Downloads/Aufgaben/Unbarer_Zahlungsverkehr/Bankleitzahlen/2018_06_03/blz_2018_03_05_txt.txt?__blob=publicationFile";
+    private final static String URL = "https://www.bundesbank.de/Redaktion/DE/Downloads/Aufgaben/Unbarer_Zahlungsverkehr/Bankleitzahlen/2018_06_03/blz_2018_03_05_txt.txt?__blob=publicationFile";
 
     private ResourceLoader resourceLoader;
 
